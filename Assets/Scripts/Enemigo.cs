47978bc13246b28b02bf5d39b4e39eacfd01f23d
Pablo <pablosanchez_964@hotmail.com>
2019-10-02 17:16:30 +0200

    Enemigo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField] Transform[] puntosDeCamino;
+    Vector3 siguientePosicion;
+    [SerializeField] float velocidad = 2f;
+    float distanciaCambio = 0.2f;
+    byte numeroSiguientePosicion;
+    void Start()
+    {
+        siguientePosicion = puntosDeCamino[0].position;
+        numeroSiguientePosicion = 0;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            siguientePosicion,
+            velocidad * Time.deltaTime);
+        if (Vector3.Distance(transform.position,
+             siguientePosicion) < distanciaCambio)
+        {
+            numeroSiguientePosicion++;
+            if (numeroSiguientePosicion >= puntosDeCamino.Length)
+                numeroSiguientePosicion = 0;
+            siguientePosicion = puntosDeCamino[numeroSiguientePosicion].
+            position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("PerderVida");
+    }
+}
+
+
+    
+
+
---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody body;
+    [SerializeField]float velocidadAvance = 200f;
+    [SerializeField] float velocidadRotac = 10f;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float avance = Input.GetAxis("Vertical")
+            * velocidadAvance * Time.deltaTime;
+        float rotacion = Input.GetAxis("Horizontal")
+        * velocidadRotac * Time.deltaTime;
+        transform.RotateAround(Vector3.up, rotacion);
+        transform.position += transform.forward
+            * Time.deltaTime * avance;
+    }
+}
